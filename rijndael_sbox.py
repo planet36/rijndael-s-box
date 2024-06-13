f28d9dccbb63a18a21fdc987aa730c6d1e605484
Steven Ward <planet36@users.noreply.github.com>
2024-06-13 08:15:54 -0400

    Add type hint annotation to functions

--- rijndael_sbox.py
+++ rijndael_sbox.py
@@ -4,7 +4,7 @@
 # x and y are nonnegative integers
 # Their associated binary polynomials are multiplied.
 # The associated integer to this product is returned.
-def multiply_ints_as_polynomials(x, y):
+def multiply_ints_as_polynomials(x: int, y: int) -> int:
     z = 0
     while x != 0:
         if x & 1 == 1:
@@ -16,7 +16,7 @@ def multiply_ints_as_polynomials(x, y):
 
 # Returns the number of bits that are used
 # to store the non-negative integer x.
-def number_bits(x):
+def number_bits(x: int) -> int:
     nb = 0
     while x != 0:
         nb += 1
@@ -26,7 +26,7 @@ def number_bits(x):
 
 # x is a nonnegative integer
 # m is a positive integer
-def mod_int_as_polynomial(x, m):
+def mod_int_as_polynomial(x: int, m: int) -> int:
     nbm = number_bits(m)
     while True:
         nbx = number_bits(x)
@@ -38,7 +38,7 @@ def mod_int_as_polynomial(x, m):
 
 # x,y are 8-bits
 # The output value is 8-bits
-def rijndael_multiplication(x, y):
+def rijndael_multiplication(x: int, y: int) -> int:
     z = multiply_ints_as_polynomials(x, y)
     m = 0b100011011
     return mod_int_as_polynomial(z, m)
@@ -47,7 +47,7 @@ def rijndael_multiplication(x, y):
 # x is 8-bits
 # The output value is 8-bits
 # Here we find the inverse just through a brute force search.
-def rijndael_inverse(x):
+def rijndael_inverse(x: int) -> int:
     if x == 0:
         return 0
     for y in range(1, 256):
@@ -57,7 +57,7 @@ def rijndael_inverse(x):
 
 # x, y are nonnegative integers
 # considered as vectors of bits
-def dot_product(x, y):
+def dot_product(x: int, y: int) -> int:
     z = x & y
     dot = 0
     while z != 0:
@@ -70,7 +70,7 @@ def dot_product(x, y):
 # 8 by 8 matrix of 0's and 1's
 # x and b are 8-bit integers, considered as column vectors
 # This function calculates A * x + b
-def affine_transformation(A, x, b):
+def affine_transformation(A: int, x: int, b: int) -> int:
     y = 0
     for i in reversed(range(8)):
         row = (A >> 8 * i) & 0xff
@@ -81,7 +81,7 @@ def affine_transformation(A, x, b):
 
 # The input value x and the output value
 # of the function are both 8-bit integers
-def rijndael_sbox(x):
+def rijndael_sbox(x: int) -> int:
     xinv = rijndael_inverse(x)
     A = 0b1111100001111100001111100001111110001111110001111110001111110001
     b = 0b01100011
